43a95164480556bd4f6dd7fd95073c43fb984c03
nguyenkevin-dotcom <nguk0907@gmail.com>
2026-02-18 10:48:37 +0100

    formatting code

--- task_cli/__main__.py
+++ task_cli/__main__.py
@@ -6,8 +6,8 @@ def main():
 
     # Introduction of my CLI
     parser = argparse.ArgumentParser(
-        description = "Task manager",
-        epilog = "Command example: \033[1m"
+        description="Task manager",
+        epilog="Command example: \033[1m"
         + 'task-cli add "Description of the task"'
         + '\033[0m => it will create a new task with description: "Description of the task"',
     )
@@ -16,36 +16,45 @@ def main():
     subparsers = parser.add_subparsers(title="Operators", dest="operator")
 
     # Command add
-    add = subparsers.add_parser("add", help = "Adding task")
-    add.add_argument("desc", type = str, help = "A short description of the task")
+    add = subparsers.add_parser("add", help="Adding task")
+    add.add_argument("desc", type=str, help="A short description of the task")
 
     # Command update
     update = subparsers.add_parser(
-        "update", help = "Updating existing task with choosing their id"
+        "update", help="Updating existing task with choosing their id"
     )
-    update.add_argument("id", type = int, help = "ID of the task")
+    update.add_argument("id", type=int, help="ID of the task")
     update.add_argument(
-        "updatedDesc", type = str, help = "A short description of the updating task"
+        "updatedDesc", type=str, help="A short description of the updating task"
     )
 
     # Command delete
-    delete = subparsers.add_parser("delete", help = "Delete existing task by id")
-    delete.add_argument("id", type = int, help = "ID of the task")
+    delete = subparsers.add_parser("delete", help="Delete existing task by id")
+    delete.add_argument("id", type=int, help="ID of the task")
 
     # Command mark-in-progress
     in_progress = subparsers.add_parser(
-        "mark-in-progress", help = "Change status from [ todo ] to [ in-progress ]"
+        "mark-in-progress", help="Change status from [ todo ] to [ in-progress ]"
     )
-    in_progress.add_argument("id", type = int, help = "ID of the task")
+    in_progress.add_argument("id", type=int, help="ID of the task")
 
     # Command mark-done
     done = subparsers.add_parser(
-        "mark-done", help = "Change status from [ todo ] to [ in-progress ]"
+        "mark-done", help="Change status from [ todo ] to [ in-progress ]"
     )
-    done.add_argument("id", type = int, help = "ID of the task")
+    done.add_argument("id", type=int, help="ID of the task")
 
-    list = subparsers.add_parser("list", help = "list created tasks, can also choose by their status [ todo | in-progress | done ]")
-    list.add_argument("type_status", type = str, help = "choose task based on status", nargs = '?', default = "")
+    list = subparsers.add_parser(
+        "list",
+        help="list created tasks, can also choose by their status [ todo | in-progress | done ]",
+    )
+    list.add_argument(
+        "type_status",
+        type=str,
+        help="choose task based on status",
+        nargs="?",
+        default="",
+    )
 
     # The text I have written
     args = parser.parse_args()
